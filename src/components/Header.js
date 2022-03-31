bccdfe9f738709f70adabf4ad3ed71722f42ac68
Awu Chen <awu.mxr@gmail.com>
2022-03-30 20:28:40 -0700

    - change routes to Link component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { connectWallet, disconnectWallet, incrementData, decrementData } from '../actions';
 
