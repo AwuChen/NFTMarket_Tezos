e046b4c154ced192bce4c5b4706b292c18eee724
Awu Chen <awu.mxr@gmail.com>
2022-03-30 20:01:16 -0700

    - updated marketplace & token address in index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-        contractAddress : "KT1XCAdyQsHuNTkeLJes7R3krmGhtsmoBvNz", 
+        contractAddress : "KT1UMVjY93bnx5SUapcs12GAQWvZgyiyDh4p", 
+        tokenAddress: "KT1GHdgQwXEBSmzf1d84gkYhoJuK9dPAP2LD"
 }
 
 export default config;
