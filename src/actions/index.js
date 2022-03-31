ea132cb5735cd28dd2389cf7473338d002a8944e
Awu Chen <awu.mxr@gmail.com>
2022-03-30 20:07:19 -0700

    - added mint NFT contract function

--- src/actions/index.js
+++ src/actions/index.js
@@ -107,3 +107,21 @@ export const decrementData = ({Tezos}) => {
         }
     }
 }
+
+
+export const mintNFT = ({ Tezos, amount, metadata }) => {
+	return async (dispatch) => {
+		try {
+			const contract = await Tezos.wallet.at(config.contractAddress);
+			let bytes = "";
+			for (var i = 0; i < metadata.length; i++) {
+				bytes += metadata.charCodeAt(i).toString(16).slice(-4);
+			}
+			const op = await contract.methods.mint(amount, bytes).send();
+			await op.confirmation();
+            dispatch(fetchData());
+		} catch (e) {
+			console.log(e);
+		}
+	};
+};
